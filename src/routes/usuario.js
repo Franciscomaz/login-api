6349c54ab6c18de0df88e670862dd5aa75985591
Francisco <chicomazfilho@hotmail.com>
2018-08-13 22:03:09 -0300

    Modificação nas rotas

--- src/routes/usuario.js
+++ src/routes/usuario.js
@@ -2,7 +2,7 @@ let usuario = require('../controllers/usuario');
 const authenticator = require('../middleware/authenticator');
 
 module.exports = server => {
-    server.post('/create-user', usuario.create);
+    server.post('/user', usuario.create);
     server.post('/logar', usuario.logar);
-    server.get('/usuarios', usuario.getAll);
+    server.get('/users', usuario.getAll);
 };
